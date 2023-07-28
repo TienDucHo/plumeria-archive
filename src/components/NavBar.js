75c77ec56720673f3e8414f171938bc2ce9fedce
Thea Nguyen <thea25060@gmail.com>
2023-07-28 02:02:31 -0600

    Added navigation bar functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,14 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const colorClass = (pathname, myPath) => {
+  return myPath === pathname ? "text-celestialBlue" : "text-black";
+};
 
 export default function NavBar() {
+  const currentPath = usePathname();
+
   return (
     <nav className="font-bold whitespace-nowrap grid grid-cols-2 mx-10 mt-8">
       <div className="font-playfair text-3xl text-left italic">
@@ -11,22 +19,52 @@ export default function NavBar() {
       <div className="justify-self-end uppercase">
         <ul className="flex gap-16">
           <li id="home">
-            <Link href="/">Home</Link>
+            <Link
+              className={colorClass(currentPath, "/")}
+              href="/"
+            >
+              Home
+            </Link>
           </li>
           <li id="news">
-            <Link href="/pages/news">News</Link>
+            <Link
+              className={colorClass(currentPath, "/pages/news")}
+              href="/pages/news"
+            >
+              News
+            </Link>
           </li>
           <li id="events">
-            <Link href="/pages/events">Events</Link>
+            <Link
+              className={colorClass(currentPath, "/pages/events")}
+              href="/pages/events"
+            >
+              Events
+            </Link>
           </li>
           <li id="organizations">
-            <Link href="/pages/organizations">Organizations</Link>
+            <Link
+              className={colorClass(currentPath, "/pages/organizations")}
+              href="/pages/organizations"
+            >
+              Organizations
+            </Link>
           </li>
           <li id="aboutUs">
-            <Link href="/pages/aboutUs">About Us</Link>
+            <Link
+              className={colorClass(currentPath, "/pages/aboutUs")}
+              href="/pages/aboutUs"
+            >
+              About Us
+            </Link>
           </li>
           <li id="contact">
-            <Link href="/pages/contact">Contact</Link>
+            <Link
+              className={colorClass(currentPath, "/pages/contact")}
+              href="/pages/contact"
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </div>
