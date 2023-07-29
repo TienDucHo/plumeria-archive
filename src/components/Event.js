c84e059c02b96bfb4f1ed9ed86bf4280adc450d4
Thea Nguyen <thea25060@gmail.com>
2023-07-29 02:19:13 -0600

    Added image styling in grid

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,32 +1,43 @@
-import { MdLocationPin } from "react-icons/md";
-import { AiOutlineClockCircle } from "react-icons/ai";
+import Calendar from "./Calendar";
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Event({ clubName, date, month, location, time }) {
   return (
-    <div className="flex gap-x-3">
-      <div
-        id="month-date-calendar"
-        className="flex text-center"
+    <div className="grid grid-cols-2 grid-rows-auto mx-16">
+      <h1 className="text-4xl font-playfair font-semibold">
+        Lorem ipsum dolor sit amet consectetur.
+      </h1>
+      <Link
+        href=""
+        className="justify-self-end"
       >
-        <div className="border-2 border-celestialBlue p-8">
-          <p className="text-4xl font-bold italic text-celestialBlue">{date}</p>
-          <p className="opacity-70 font-bold italic">{month}</p>
-        </div>
+        See more events
+      </Link>
+      <p className="opacity-70 self-center">
+        Lorem ipsum dolor sit amet consectetur. Tincidunt platea vulputate
+        egestas eu.
+      </p>
+
+      <div className="row-start-3">
+        <Calendar
+          clubName={clubName}
+          date={date}
+          month={month}
+          location={location}
+          time={time}
+        ></Calendar>
       </div>
-      <div
-        id="location-time-display"
-        className="font-semibold"
-      >
-        <h1 className="text-3xl text-celestialBlue">{clubName}</h1>
-        <p className="flex items-center gap-x-2">
-          <MdLocationPin></MdLocationPin>
-          {location}
-        </p>
-        <p className="flex items-center gap-x-2">
-          <AiOutlineClockCircle></AiOutlineClockCircle>
-          {time}
-        </p>
+      <button className="row-start-4 bg-celestialBlue text-white">
+        Register
+      </button>
+      <div className="col-start-2 row-span-3 justify-self-center relative w-full h-full">
+        <Image
+          className=" object-cover "
+          src="/pic1.jpg"
+          alt=""
+          fill
+        ></Image>
       </div>
     </div>
   );
