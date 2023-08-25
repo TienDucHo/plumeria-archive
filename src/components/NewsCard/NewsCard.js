84f177bd726de948e0ec69e0c0cb6bda9ba570d6
Thea Nguyen <thea25060@gmail.com>
2023-08-24 19:28:18 -0600

    Refined News Card layout

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -4,7 +4,7 @@ const NewsCard = () => {
   return (
     <div className="relative flex flex-col w-[24rem] text-white px-2 pt-32 pb-2">
       {/* filter */}
-      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
 
       <Image
         className="object-cover z-[-1]"
@@ -13,12 +13,12 @@ const NewsCard = () => {
         fill
       ></Image>
 
-      <div className="z-10">
+      <div className="z-10 flex flex-col gap-y-2">
         <div className="flex gap-x-12">
           <p className="text-aetroBlue font-semibold">Harmonica</p>
           <p className="opacity-60 text-sm">10 minutes ago</p>
         </div>
-        <h1 className=" text-2xl font-playfair">
+        <h1 className=" text-2xl font-playfair font-semibold">
           Lorem ipsum dolor sit amet consectetur.
         </h1>
         <p className="opacity-70 text-sm">
