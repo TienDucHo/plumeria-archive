4cf8e7fbc4adca87dafbc02ea4d131cb560c70e7
Thea Nguyen <thea25060@gmail.com>
2023-08-24 18:56:36 -0600

    Fixed indicator carousel

--- src/components/MyCarousel/MyCarousel.js
+++ src/components/MyCarousel/MyCarousel.js
@@ -7,6 +7,7 @@ import {
   getOriginalIndexLookupTableByClones,
   getOriginalCounterPart,
 } from "react-multi-carousel/lib/utils/clones";
+import { Fade } from "react-awesome-reveal";
 
 // components
 import Event from "../Event/Event";
@@ -28,72 +29,72 @@ const responsive = {
 
 const MyCarousel = ({ items }) => {
   let carouselRef = null;
-  let indicatorWidth = `w-1/${items.length}`;
   const [nextSlide, setNextSlide] = useState(0);
 
   return (
-    <div>
-      <Carousel
-        ref={(el) => (carouselRef = el)}
-        responsive={responsive}
-        infinite={true}
-        autoPlay={true}
-        autoPlaySpeed={4600}
-        arrows={false}
-        beforeChange={(nextSlide) => {
-          if (carouselRef) {
-            let cloneTable = getOriginalIndexLookupTableByClones(
-              carouselRef.state.slidesToShow,
-              React.Children.toArray(carouselRef.props.children)
-            );
-            setNextSlide(cloneTable[nextSlide]);
-          }
-        }}
-      >
-        {items.map((elem, index) => {
-          return (
-            <Event
-              key={index}
-              title={elem.title}
-              subTitle={elem.subTitle}
-              club={elem.club}
-              time={elem.time}
-              date={elem.date}
-              month={elem.month}
-              location={elem.location}
-              imgSource={elem.imgSource}
-            />
-          );
-        })}
-      </Carousel>
-
-      <div className="flex flex-col mx-4 sm:mx-8 md:grid md:grid-cols-2 md:gap-x-4">
-        <div className="flex mt-1 justify-center gap-x-2 md:col-start-1 md:ml-0 lg:ml-4 lg:max-w-4xl">
-          {items.map((_elem, index) => {
+    <Fade triggerOnce>
+      <div>
+        <Carousel
+          ref={(el) => (carouselRef = el)}
+          responsive={responsive}
+          infinite={true}
+          autoPlay={true}
+          autoPlaySpeed={4600}
+          arrows={false}
+          beforeChange={(nextSlide) => {
+            if (carouselRef) {
+              let cloneTable = getOriginalIndexLookupTableByClones(
+                carouselRef.state.slidesToShow,
+                React.Children.toArray(carouselRef.props.children)
+              );
+              setNextSlide(cloneTable[nextSlide]);
+            }
+          }}
+        >
+          {items.map((elem, index) => {
             return (
-              <div
+              <Event
                 key={index}
-                className={
-                  nextSlide === index
-                    ? `h-2 bg-saffronYellow opacity-90 cursor-pointer ${indicatorWidth}`
-                    : `h-2 bg-black opacity-90 cursor-pointer ${indicatorWidth}`
-                }
-                onClick={() => {
-                  if (carouselRef) {
-                    let afterClonedIndex = getOriginalCounterPart(
-                      index,
-                      carouselRef.state,
-                      React.Children.toArray(carouselRef.props.children)
-                    );
-                    carouselRef.goToSlide(afterClonedIndex);
-                  }
-                }}
-              ></div>
+                title={elem.title}
+                subTitle={elem.subTitle}
+                club={elem.club}
+                time={elem.time}
+                date={elem.date}
+                month={elem.month}
+                location={elem.location}
+                imgSource={elem.imgSource}
+              />
             );
           })}
+        </Carousel>
+        <div className="test-classes flex flex-col mx-4 sm:mx-8 md:grid md:grid-cols-2 md:gap-x-4">
+          <div className="flex mt-1 justify-between gap-x-2 md:col-start-1 md:ml-0 lg:ml-4 lg:max-w-4xl">
+            {items.map((_elem, index) => {
+              return (
+                <div
+                  key={index}
+                  className={`h-2 flex-grow bg-black opacity-90 cursor-pointer ${
+                    nextSlide === index ? "bg-saffronYellow" : "bg-black"
+                  }`}
+                  onClick={() => {
+                    if (carouselRef) {
+                      let afterClonedIndex = getOriginalCounterPart(
+                        index,
+                        carouselRef.state,
+                        React.Children.toArray(carouselRef.props.children)
+                      );
+                      carouselRef.goToSlide(afterClonedIndex);
+                    }
+                  }}
+                >
+                  &nbsp;
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </Fade>
   );
 };
 
