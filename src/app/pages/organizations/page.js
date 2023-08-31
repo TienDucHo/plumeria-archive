d133f7a75b0fc3e99207314c3ce57ad938d406e9
Thea Nguyen <thea25060@gmail.com>
2023-08-31 15:38:08 -0600

    Added responsive layout for org page

--- src/app/pages/organizations/page.js
+++ src/app/pages/organizations/page.js
@@ -2,9 +2,62 @@
 import OrgCard from "@/components/Organization/OrgCard";
 
 const Organizations = () => {
+  const orgInfo = [
+    {
+      club: "Harmonica PTNK Club",
+      char1: "Music",
+      char2: "Instrument",
+      content:
+        "Lorem ipsum dolor sit amet consectetur. Condimentum phasellus eleifend diam viverra ornare metus id eleifend. Eget facilisi fusce",
+      imgSource: "/pic1.jPg",
+    },
+    {
+      club: "TPT",
+      char1: "Events",
+      char2: "News",
+      content:
+        "Lorem ipsum dolor sit amet consectetur. Condimentum phasellus eleifend diam viverra ornare metus id eleifend. Eget facilisi fusce",
+      imgSource: "/pic2.jpg",
+    },
+    {
+      club: "Define Magic Club",
+      char1: "Magic",
+      char2: "Tricks",
+      content:
+        "Lorem ipsum dolor sit amet consectetur. Condimentum phasellus eleifend diam viverra ornare metus id eleifend. Eget facilisi fusce",
+      imgSource: "/pic3.jpg",
+    },
+    {
+      club: "MIB",
+      char1: "Dance",
+      char2: "Music",
+      content:
+        "Lorem ipsum dolor sit amet consectetur. Condimentum phasellus eleifend diam viverra ornare metus id eleifend. Eget facilisi fusce",
+      imgSource: "/pic1.jpg",
+    },
+    {
+      club: "FZone",
+      char1: "Dance",
+      char2: "Music",
+      content:
+        "Lorem ipsum dolor sit amet consectetur. Condimentum phasellus eleifend diam viverra ornare metus id eleifend. Eget facilisi fusce",
+      imgSource: "/pic2.jpg",
+    },
+  ];
   return (
-    <div>
-      <OrgCard />
+    <div className="mx-4 mb-16 grid gap-y-[2rem] sm:mx-8 sm:gap-x-[2%] sm:grid-cols-2 sm:gap-y-[4rem] lg:gap-y-[8rem] lg:mx-12 lg:grid-cols-3">
+      {orgInfo.map((elem, index) => {
+        return (
+          <OrgCard
+            key={index}
+            club={elem.club}
+            content={elem.content}
+            imgSource={elem.imgSource}
+            char1={elem.char1}
+            char2={elem.char2}
+          />
+        );
+      })}
     </div>
   );
 };
--- src/components/Organization/OrgCard.js
+++ src/components/Organization/OrgCard.js
@@ -1,24 +1,28 @@
 import Image from "next/image";
 
-const OrgCard = () => {
+const OrgCard = ({ club, content, imgSource, char1, char2 }) => {
   return (
-    <div className="flex flex-col items-center w-80 gap-y-2">
-      <div className="relative w-full h-80 pt-32">
+    <div className="flex flex-col items-center w-full gap-y-2">
+      <div className="relative w-full pt-[60%] lg:pt-[80%]">
         <Image
-          src="/pic1.jpg"
+          className="object-cover"
+          src={imgSource}
           alt="Organization Image"
           fill
         ></Image>
       </div>
-      <h1 className="font-playfair text-2xl font-semibold">To Phat Thanh</h1>
-      <div className="flex gap-x-8">
-        <p className="bg-celestialBlue px-2 text-white font-semibold">Media</p>
-        <p className="bg-celestialBlue px-2 text-white font-semibold">Events</p>
+      <h1 className="font-playfair text-xl md:text-2xl font-semibold">
+        {club}
+      </h1>
+      <div className="flex gap-x-8 items-center">
+        <p className="bg-celestialBlue px-2 h-6 text-white flex items-center font-semibold text-sm md:text-base">
+          {char1}
+        </p>
+        <p className="bg-celestialBlue px-2 h-6 text-white font-semibold flex items-center">
+          {char2}
+        </p>
       </div>
-      <p className="text-center">
-        Lorem ipsum dolor sit amet consectetur. Condimentum phasellus eleifend
-        diam viverra ornare metus id eleifend. Eget facilisi fusce{" "}
-      </p>
+      <p className="text-center text-sm md:text-base">{content}</p>
     </div>
   );
 };
