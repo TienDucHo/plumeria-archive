1088a8d2e565e2a4ea7449933a90e63dd2c926f9
Thea Nguyen <thea25060@gmail.com>
2023-09-04 23:56:02 -0600

    Refined about page layout

--- src/app/pages/aboutUs/page.js
+++ src/app/pages/aboutUs/page.js
@@ -9,12 +9,12 @@ import AboutAnimation from "@/components/AboutAnimation/AboutAnimation";
 const AboutUs = () => {
   return (
     <div>
-      <div className="sm:hidden flex flex-col items-center gap-y-8 md:gap-y-16 mx-4 mb-16 sm:mx-8 lg:mx-12">
+      <div className="md:hidden flex flex-col items-center gap-y-8 md:gap-y-16 mx-4 mb-16 sm:mx-8 lg:mx-12">
         <Fade
           delay={500}
           triggerOnce
         >
-          <div className="grid items-center grid-rows-auto grid-cols-1 sm:grid-cols-2 w-full">
+          <div className="grid items-center grid-rows-auto grid-cols-1 md:grid-cols-2 w-full">
             <div className="relative w-full h-full pt-64">
               <Image
                 className="object-cover row-start-1 col-start-1"
@@ -23,13 +23,13 @@ const AboutUs = () => {
                 fill
               />
             </div>
-            <p className="text-sm mt-4 mb-24 text-center sm:my-0 sm:text-start sm:row-start-1 sm:col-start-2 sm:pl-4 md:text-base">
+            <p className="text-sm mt-4 mb-24 text-center md:my-0 md:text-start md:row-start-1 md:col-start-2 md:pl-4 md:text-base">
               Lorem ipsum dolor sit amet consectetur. Pulvinar posuere sagittis
               sed convallis nibh. Suspendisse pulvinar morbi pulvinar elementum
               adipiscing. Egestas elit aenean et eget sed bibendum vitae tempor.
               Porttitor curabitur ipsum donec natoque feugiat vitae integer.
             </p>
-            <p className="text-sm mt-4 mb-24 text-center sm:my-0 sm:text-start row-start-4 sm:row-start-2 sm:col-start-1 sm:pr-4 md:text-base">
+            <p className="text-sm mt-4 mb-24 text-center md:my-0 md:text-start row-start-4 md:row-start-2 md:col-start-1 md:pr-4 md:text-base">
               Lorem ipsum dolor sit amet consectetur. Pulvinar posuere sagittis
               sed convallis nibh. Suspendisse pulvinar morbi pulvinar elementum
               adipiscing. Egestas elit aenean et eget sed bibendum vitae tempor.
@@ -48,6 +48,7 @@ const AboutUs = () => {
       </div>
       <AboutAnimation className="hidden md:block" />
       <Fade
+        triggerOnce
         duration={2000}
         cascade
         className="w-full flex flex-col items-center mb-4 md:mb-12"
