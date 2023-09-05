bb77e436f28d82082f5f69954095029919512c37
Thea Nguyen <thea25060@gmail.com>
2023-09-04 22:55:05 -0600

    Added responsiveness to layout

--- src/app/pages/aboutUs/page.js
+++ src/app/pages/aboutUs/page.js
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 const AboutUs = () => {
   return (
-    <div className="flex flex-col items-center gap-y-8 md:gap-y-16 mx-4 mb-16">
+    <div className="flex flex-col items-center gap-y-8 md:gap-y-16 mx-4 mb-16 sm:mx-8 lg:mx-12">
       <h1 className="font-playfair text-celestialBlue text-3xl sm:text-4xl md:text-5xl">
         About Us
       </h1>
-      <div className="grid items-center grid-rows-auto grid-cols-1 sm:grid-cols-2 w-[80%] lg:w-[60%]">
-        <div className="relative  aspect-square">
+      <div className="grid items-center grid-rows-auto grid-cols-1 sm:grid-cols-2 w-full">
+        <div className="relative w-full h-full pt-64">
           <Image
-            className="object-cover row-start-1 col-start-1 "
+            className="object-cover row-start-1 col-start-1"
             src="/pic5.png"
             alt="About Image 1"
             fill
@@ -28,7 +28,7 @@ const AboutUs = () => {
           adipiscing. Egestas elit aenean et eget sed bibendum vitae tempor.
           Porttitor curabitur ipsum donec natoque feugiat vitae integer.
         </p>
-        <div className="relative aspect-square">
+        <div className="relative w-full h-full pt-64">
           <Image
             className="object-cover row-start-3 md:row-start-2 md:col-start-2"
             src="/pic6.png"
