1fe293b6126827b2b21064b6ada760327c6eb613
Thea Nguyen <thea25060@gmail.com>
2023-09-04 21:19:37 -0600

    Added mobile layout for org card

--- src/components/Organization/OrgCard.js
+++ src/components/Organization/OrgCard.js
@@ -5,9 +5,9 @@ const OrgCard = ({ club, content, imgSource, char1, char2 }) => {
   return (
     <Link
       href="/pages/organizations"
-      className="flex flex-col items-center w-full gap-y-2 md:gap-y-4 group"
+      className="grid grid-cols-2  gap-x-4 place-items-center sm:flex flex-col items-center w-full gap-y-2 md:gap-y-4 group"
     >
-      <div className="relative w-full aspect-square">
+      <div className="relative w-full aspect-square row-span-2">
         <Image
           className="object-cover"
           src={imgSource}
@@ -15,18 +15,23 @@ const OrgCard = ({ club, content, imgSource, char1, char2 }) => {
           fill
         ></Image>
       </div>
-      <h1 className="font-playfair text-lg md:text-xl font-semibold group-hover:text-aetroBlue transition ease-in-out">
-        {club}
-      </h1>
-      <div className="flex gap-x-8 items-center">
-        <p className="bg-celestialBlue px-2 h-6 text-white flex items-center font-semibold text-sm md:text-base">
-          {char1}
-        </p>
-        <p className="bg-celestialBlue px-2 h-6 text-white font-semibold flex items-center">
-          {char2}
+      <div className="row-span-2 h-full sm:h-auto relative flex flex-col w-full self-start sm:items-center gap-y-2 transition ease-in-out">
+        <h1 className="font-playfair text-lg md:text-xl font-semibold group-hover:text-aetroBlue">
+          {club}
+        </h1>
+        <p className="sm:hidden text-sm md:text-base">{content}</p>
+        <div className="absolute bottom-0 left-0 flex gap-x-8 items-center sm:static">
+          <p className="bg-celestialBlue px-2 h-6 text-white flex items-center font-semibold text-sm md:text-base">
+            {char1}
+          </p>
+          <p className="bg-celestialBlue px-2 h-6 text-white font-semibold flex items-center">
+            {char2}
+          </p>
+        </div>
+        <p className="hidden sm:flex text-center text-sm md:text-base">
+          {content}
         </p>
       </div>
-      <p className="text-center text-sm md:text-base">{content}</p>
     </Link>
   );
 };
