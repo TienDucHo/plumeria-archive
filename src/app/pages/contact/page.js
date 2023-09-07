96eea801c9d7faf6940b6a0be71f0f74e1529585
Thea Nguyen <thea25060@gmail.com>
2023-09-06 18:56:00 -0600

    Added Contact us layout

--- src/app/pages/contact/page.js
+++ src/app/pages/contact/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 // components
 import ContactHero from "@/components/Hero/ContactHero";
 
@@ -7,8 +9,12 @@ const Contact = () => {
     <div className="mb-16">
       <ContactHero />
       <div className="flex flex-col items-center my-16">
-        <p className="text-celestialBlue text-4xl font-semibold font-playfair mb-12">
+        <p className="text-celestialBlue text-3xl sm:text-4xl md:text-5xl font-playfair mb-8">
           How can we help?
+        </p>
+        <p className="text-base md:text-xl mb-12 opacity-70 w-[60%] lg:w-[30%] text-center">
+          Should you have any questions, feel free to contact us anytime. We
+          will get back to you as soon as we can.
         </p>
         <form className="bg-white flex flex-col gap-y-8">
           {inputs.map((elem, index) => {
@@ -19,13 +25,13 @@ const Contact = () => {
               >
                 <label
                   for={elem}
-                  className="font-playfair text-celestialBlue text-xl pr-4 font-semibold"
+                  className="font-playfair text-celestialBlue md:text-xl pr-4 "
                 >
                   {elem}
                 </label>
                 <input
-                  className="custom-input w-96"
-                  type="text"
+                  className="custom-input w-96 md:text-xl"
+                  type={elem === "Name" ? "text" : "email"}
                   name={elem}
                   id={elem}
                 />
@@ -34,18 +40,26 @@ const Contact = () => {
           })}
           <label
             for="Message"
-            className="font-playfair text-celestialBlue text-xl pr-4 font-semibold"
+            className="font-playfair text-celestialBlue md:text-xl pr-4"
           >
             Message
           </label>
           <textarea
-            maxLength={110}
+            className="md:text-xl"
+            maxLength={88}
             type="text"
             name="Message"
             id="message"
             cols={28}
             rows={2}
           />
+          <button
+            id="form-button"
+            onClick={() => alert("Submitted")}
+            className="btn btn-primary rounded-none capitalize text-lg md:text-xl text-white border-none"
+          >
+            Submit
+          </button>
         </form>
       </div>
     </div>
